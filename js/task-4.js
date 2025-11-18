e86a2f2aee9ec00458ba4c7552a19da795f26b5e
Olha_Roman <ovgrozdova79@gmail.com>
2025-11-18 22:47:40 +0100

    bravo

--- js/task-4.js
+++ js/task-4.js
@@ -3,3 +3,11 @@ const planets = ['Earth', 'Mars', 'Venus'];
 console.log(planets[0]); // 'Earth'
 console.log(planets[1]); // 'Mars'
 console.log(planets[2]); // 'Venus'
+
+function getExtremeelements(array) {
+    const firstElement = array[0];
+    const lastElementIndex = array.length - 1;
+    const lastElement = array[lastElementIndex];
+    return [firstElement, lastElement];
+}
+console.log(getExtremeelements(["cat", "dog", "dug", "hors"]));
