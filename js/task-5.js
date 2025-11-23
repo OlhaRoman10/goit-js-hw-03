129be2b82b7c23d78d8fc1525ca4380b90ad45f7
Olha_Roman <ovgrozdova79@gmail.com>
2025-11-23 22:03:40 +0100

    metod3

--- js/task-5.js
+++ js/task-5.js
@@ -6,3 +6,12 @@ function getEngravingPrice(message, pricePerWort) {
 console.log(getEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
 console.log(getEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
 
+function createArrayofNumbers(min, max) {
+    const numbers = [];
+    for (let i = min; i <= max; i += 1) {
+        numbers.push(i);
+    }       
+    return numbers;
+}
+console.log(createArrayofNumbers(1, 3)); // [1, 2, 3]
+console.log(createArrayofNumbers(14, 17)); // [14, 15, 16, 17]
