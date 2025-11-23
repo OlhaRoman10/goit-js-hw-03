ca3627f51c63ddb1d8c449ddeeed57fd1714d25c
Olha_Roman <ovgrozdova79@gmail.com>
2025-11-23 17:42:23 +0100

    arrays

--- js/task-4.js
+++ js/task-4.js
@@ -6,8 +6,8 @@ console.log(planets[2]); // 'Venus'
 
 function getExtremeelements(array) {
     const firstElement = array[0];
-    const lastElementIndex = array.length - 1;
-    const lastElement = array[lastElementIndex];
+    
+    const lastElement = array[array.length - 1];
     return [firstElement, lastElement];
 }
 console.log(getExtremeelements(["cat", "dog", "dug", "hors"]));
