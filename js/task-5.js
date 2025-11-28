db44084f7e41141724609930c2210bdad4744655
Olha_Roman <ovgrozdova79@gmail.com>
2025-11-28 21:22:30 +0100

    addition

--- js/task-5.js
+++ js/task-5.js
@@ -37,4 +37,26 @@ function getCommonElements(array1, array2) {
     }   
 
 console.log(getCommonElements([1, 2, 3], [2, 4])); // [2]
-console.log(getCommonElements(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple'])); // ['apple', 'banana']      
+console.log(getCommonElements(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple'])); // ['apple', 'banana']
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4.7,
+  price: 5000,
+  tags: ["premium", "promoted", "top", "trusted"],
+  owner: {
+    name: "Henry Sibola",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
+};
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location;
+apartment.location = {
+  country: "Jamaica",
+  city: "Kingston",
+};
+
+console.log(apartment);
